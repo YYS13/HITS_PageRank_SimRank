7071b89625cb315e1de9d90d050961eada6b2ff8
YangYo <a0952742534@gmail.com>
2023-12-14 00:32:24 +0800

    ready for test

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ import os
 from src.utils import *
 
 data_path = os.getcwd() + "/inputs/"
-files = ["graph_1.txt", "graph_2.txt", "graph_3.txt", "graph_4.txt", "graph_5.txt", "graph_6.txt"]
+files = ["graph_1.txt", "graph_2.txt", "graph_3.txt", "graph_4.txt", "graph_5.txt", "graph_6.txt", "ibm-5000.txt"]
 #create 7 graphs(graph_1, graph_2, graph_3, graph_4, graph_5, graph_6, graph_7)
 for file in files:
     if file[:3] == "gra":
-        globals()[file[:7]] = create_graph(data_path + file)
+        globals()[file[:7]] = create_graph(data_path + file, False)
     else:
-        globals()[file[:3]] = create_graph(data_path + file)
+        globals()[file[:3]] = create_graph(data_path + file, True)
+
+
+HITS(ibm, 30)
+pageRank(ibm, 30)
+simRank(graph_1, 30)
--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,33 @@
 from src.graph import *
 
 
-def create_graph(data_path):
+def create_graph(data_path, transaction_case):
     gragh = Graph()
     with open(data_path) as file:
-        for line in file:
-            [parent, child] = line.strip().split(",")
-            gragh.link(parent, child)
+        if transaction_case:
+            for line in file:
+                [ignore, parent, child] = line.split()
+                gragh.link(parent, child)
+        else:    
+            for line in file:
+                [parent, child] = line.strip().split(",")
+                gragh.link(parent, child)
     
     return gragh
+
+def HITS(graph, iter):
+    for i in range(iter):
+        graph.HITS_update()
+    print([node.authority for node in graph.nodes])
+    print([node.hub for node in graph.nodes])
+    
+def pageRank(graph, iter):
+    for i in range(iter):
+        graph.pagerank_update()
+    print([node.pagerank for node in graph.nodes])
+
+def simRank(graph, iter):
+    graph.init_sim()
+    for i in range(iter):
+        graph.simRank_update()
+    graph.printSimMatrix()
